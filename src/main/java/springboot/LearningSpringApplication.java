86be05014266ec60f31df1cf2b40a5f214d83822
ZhamMichelle <kettebekovanyc@gmail.com>
2021-04-16 10:43:11 +0600

    component-scan

--- src/main/java/springboot/LearningSpringApplication.java
+++ src/main/java/springboot/LearningSpringApplication.java
@@ -1,11 +1,13 @@
-package com.example.learningspring;
+package springboot;
 
 import com.example.learningspring.controllers.GreetingController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.example.learningspring")
 public class LearningSpringApplication {
 
     public static void main(String[] args) {
